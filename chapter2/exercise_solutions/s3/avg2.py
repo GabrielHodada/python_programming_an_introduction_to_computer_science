3b351b9ba3a8852b02550e416c4d4853e1278e32
GabrielHodada <35962985+GabrielHodada@users.noreply.github.com>
2019-10-23 06:05:49 -0400

    Update avg2.py

--- chapter2/exercise_solutions/s3/avg2.py
+++ chapter2/exercise_solutions/s3/avg2.py
@@ -1,1 +1,13 @@
+"""
+3. Modify the avg2.py program (Section 2.5.3) to find the average of three
+exam scores.
+"""
 
+def main () :
+
+    print ("This program computes the average of two exam scores. ")
+    score1, score2, score3 = eval (raw_input ("Enter three scores separated by a comma: ") )
+    average = (score1 + score2 +score3) /3
+    print ("The average of the scores is: ", average)
+
+main () 
