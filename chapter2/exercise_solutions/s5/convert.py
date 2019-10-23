829858a29e795a243ae621ddf8bc3cd35d70cfed
GabrielHodada <35962985+GabrielHodada@users.noreply.github.com>
2019-10-23 06:06:58 -0400

    Update convert.py

--- chapter2/exercise_solutions/s5/convert.py
+++ chapter2/exercise_solutions/s5/convert.py
@@ -1,1 +1,20 @@
+"""5. Modify the convert.py program (Section 2.2) so that it computes and
+prints a table of Celsius temperatures and the Fahrenheit equivalents every
+10 degrees from 0°C to 100°C.
+"""
+
+def main () :
+    c = 0
+    f = 0
+    for i in range(11):
+
+        f = (9/5)*c+32
+        print("%3s" % c + '%15s' % f)
+
+        c+=10  
+      
+    celsius = float(input ("What is the Celsius temperature? ") )
+    fahrenheit = 9/5 * celsius + 32
+    print ("The temperature is", fahrenheit, "degrees Fahrenheit.")
+main ()
 
