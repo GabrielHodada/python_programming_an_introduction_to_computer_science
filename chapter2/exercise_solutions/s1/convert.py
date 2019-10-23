c7c57a393e32b243be6bc8b7b7a23eec576e3cb3
GabrielHodada <35962985+GabrielHodada@users.noreply.github.com>
2019-10-23 06:04:03 -0400

    Update convert.py

--- chapter2/exercise_solutions/s1/convert.py
+++ chapter2/exercise_solutions/s1/convert.py
@@ -1,1 +1,13 @@
+"""
+1. A user-friendly program should print an introduction that tells the user
+what the program does. Modify the convert.py program (Section 2.2) to
+print an introduction.
+""""
 
+
+def main () :
+    print("welcome, this app allows you to convert your temperature from fahrenheit to celsius")
+    celsius = eval (raw_input ("What is the Celsius temperature? ") )
+    fahrenheit = 9/5 * celsius + 32
+    print ("The temperature is", fahrenheit, "degrees Fahrenheit.")
+main ()
