524c2026f9cd1ffe444c1816b87a67dfc9f11909
GabrielHodada <35962985+GabrielHodada@users.noreply.github.com>
2019-10-23 06:04:44 -0400

    Update convert.py

--- chapter2/exercise_solutions/s2/convert.py
+++ chapter2/exercise_solutions/s2/convert.py
@@ -1,1 +1,21 @@
+"""
+2. On many systems with Python, it is possible to run a program by simply
+clicking (or double-clicking) on the icon of the program file. If you are
+able to run the convert.py program this way, you may discover another
+usability issue. The program starts running in a new window, but as soon
+as the program has finished, the window disappears so that you cannot
+read the results. Add an input statement at the end of the program so
+that it pauses to give the user a chance to read the results. Something like
+this should work:
+input ("Press the <Enter> key to quit.")
+"""
 
+def main () :
+
+    celsius = eval(input("What is the Celsius temperature? ") )
+    fahrenheit = 9/5 * celsius + 32
+    print ("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+    if input("Press the <Enter> key to quit.") == "":
+        quit()
+main ()
